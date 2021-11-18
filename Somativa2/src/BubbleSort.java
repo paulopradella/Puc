f0f68f3347a31b02e77a5e9dcfa8dec649f016ce
Paulo Henrique <76111995+paulopradella@users.noreply.github.com>
2021-11-18 11:36:51 -0300

    update
    Mudanças em nomenclaturas de variavei

--- Somativa2/src/BubbleSort.java
+++ Somativa2/src/BubbleSort.java
@@ -1,35 +1,27 @@
 public class BubbleSort {
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        int temp;
+    static void bubbleSort(int[] b) {
+        int n = b.length;
+        int base;
         for(int i=0; i < n; i++){
             for(int j=1; j < (n-i); j++){
-                if(arr[j-1] > arr[j]){
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
+                if(b[j-1] > b[j]){
+                    base = b[j-1];
+                    b[j-1] = b[j];
+                    b[j] = base;
+                }}}}
 
-            }
-        }
-
-    }
     public static void main(String[] args) {
-        int[] arr ={3,6,8,1,4,9,0};
+        int[] b ={3,6,8,1,4,9,0};
 
-        System.out.println("Array antes do Bubble Sort:\n");
-        for (int k : arr) {
+        System.out.println("Array antes do uso do Bubble Sort:\n");
+        for (int k : b) {
             System.out.print(k + " ");
         }
         System.out.println();
 
-        bubbleSort(arr);
+        bubbleSort(b);
 
-        System.out.println("Array depois Bubble Sort:\n");
-        for (int j : arr) {
+        System.out.println("Array depois do uso do Bubble Sort:\n");
+        for (int j : b) {
             System.out.print(j + " ");
-        }
-
-    }
-
-}
+        }}}
--- Somativa2/src/SelectionSort.java
+++ Somativa2/src/SelectionSort.java
@@ -1,32 +1,28 @@
 public class SelectionSort {
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
+    public static void selectionSort(int[] s){
+        for (int i = 0; i < s.length - 1; i++)
         {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;
-                }
-            }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
-        }
-    }
+            int base = i;
+            for (int j = i + 1; j < s.length; j++){
+                if (s[j] < s[base]){
+                    base = j;
+                }}
+            int smallerNumber = s[base];
+            s[base] = s[i];
+            s[i] = smallerNumber;
+        }}
 
     public static void main(String[] a){
-        int[] arr1 = {3,6,8,1,4,9,0};
-        System.out.println("Array antes do Selection Sort:\n");
-        for(int i:arr1){
+        int[] s1 = {3,6,8,1,4,9,0};
+        System.out.println("Array antes do uso do Selection Sort:");
+        for(int i:s1){
             System.out.print(i+" ");
         }
         System.out.println();
 
-        selectionSort(arr1);
+        selectionSort(s1);
 
-        System.out.println("Array depois do Selection Sort:\n");
-        for(int i:arr1){
+        System.out.println("\nArray depois do uso do Selection Sort:");
+        for(int i:s1){
             System.out.print(i+" ");
-        }
-    }
-}
+        }}}
